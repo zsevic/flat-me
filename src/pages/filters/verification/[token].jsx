34ccafcd19ef72527604f14fd53f4dc3868ddb17
zsevic <zsevic95@gmail.com>
2021-07-16 22:23:57 +0200

    Add loader to verification pages

--- src/pages/filters/verification/[token].jsx
+++ src/pages/filters/verification/[token].jsx
@@ -1,6 +1,6 @@
-import { message } from "antd";
+import { message, Spin } from "antd";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "components/Header";
 import { VERIFICATION_PAGE_NOTIFICATION_DURATION } from "constants/config";
 import * as filtersService from "services/filters";
@@ -8,21 +8,35 @@ import * as filtersService from "services/filters";
 const FilterVerificationPage = () => {
   const router = useRouter();
   const { token } = router.query;
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    filtersService
-      .verifyFilter(token)
-      .then(
-        () => message.success("Pretraga je uspešno sačuvana"),
-        VERIFICATION_PAGE_NOTIFICATION_DURATION
-      )
-      .catch(
-        () => message.error("Čuvanje pretrage nije uspešno obavljeno"),
-        VERIFICATION_PAGE_NOTIFICATION_DURATION
-      );
-  }, []);
+  useEffect(
+    () =>
+      filtersService
+        .verifyFilter(token)
+        .then(() => {
+          message.success(
+            "Pretraga je uspešno sačuvana",
+            VERIFICATION_PAGE_NOTIFICATION_DURATION
+          );
+          setLoading(false);
+        })
+        .catch(() => {
+          message.error(
+            "Čuvanje pretrage nije uspešno obavljeno",
+            VERIFICATION_PAGE_NOTIFICATION_DURATION
+          );
+          setLoading(false);
+        }),
+    []
+  );
 
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Spin spinning={loading} className="flex justify-center" />
+    </>
+  );
 };
 
 export default FilterVerificationPage;
--- src/pages/users/verification/[token].jsx
+++ src/pages/users/verification/[token].jsx
@@ -1,6 +1,6 @@
-import { message } from "antd";
+import { message, Spin } from "antd";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "components/Header";
 import { VERIFICATION_PAGE_NOTIFICATION_DURATION } from "constants/config";
 import * as usersService from "services/users";
@@ -8,21 +8,35 @@ import * as usersService from "services/users";
 const UserVerificationPage = () => {
   const router = useRouter();
   const { token } = router.query;
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    usersService
-      .verifyUser(token)
-      .then(
-        () => message.success("Mejl je uspešno potvrđen"),
-        VERIFICATION_PAGE_NOTIFICATION_DURATION
-      )
-      .catch(
-        () => message.error("Mejl nije potvrđen"),
-        VERIFICATION_PAGE_NOTIFICATION_DURATION
-      );
-  }, []);
+  useEffect(
+    () =>
+      usersService
+        .verifyUser(token)
+        .then(() => {
+          message.success(
+            "Mejl je uspešno potvrđen",
+            VERIFICATION_PAGE_NOTIFICATION_DURATION
+          );
+          setLoading(false);
+        })
+        .catch(() => {
+          message.error(
+            "Mejl nije potvrđen",
+            VERIFICATION_PAGE_NOTIFICATION_DURATION
+          );
+          setLoading(false);
+        }),
+    []
+  );
 
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Spin spinning={loading} className="flex justify-center" />
+    </>
+  );
 };
 
 export default UserVerificationPage;
