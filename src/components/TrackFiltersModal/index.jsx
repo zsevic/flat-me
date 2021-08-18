1c3572ae23e2196cf6d7df5cafdb649cbc866610
zsevic <zsevic95@gmail.com>
2021-08-18 02:48:55 +0200

    Remove SSEE

--- src/components/TrackFiltersModal/index.jsx
+++ src/components/TrackFiltersModal/index.jsx
@@ -1,11 +1,9 @@
 import { MailOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Modal, Steps, message } from "antd";
+import { Button, Form, Input, Modal, message } from "antd";
 import { TRACK_FILTERS_MODAL_TITLE } from "constants/config";
 import React, { useEffect, useState } from "react";
 import * as filtersService from "services/filters";
 import eventBus from "utils/event-bus";
-
-const { Step } = Steps;
 
 const formItemLayout = {
   labelCol: {
@@ -16,71 +14,30 @@ const formItemLayout = {
   },
 };
 
-const steps = [
-  {
-    title: "E-mail",
-    description: "Unesi e-mail adresu",
-  },
-  {
-    title: "Čuvanje",
-    description: "Potvrdi sačuvanu pretragu",
-  },
-  {
-    title: "Kraj",
-    description: "Pretraga je sačuvana",
-  },
-];
-
 export const TrackFiltersModal = () => {
   const [isDisabled, setIsDisabled] = useState(true);
   const [filters, setFilters] = useState({});
   const [visible, setVisible] = useState(false);
   const [form] = Form.useForm();
 
-  const [current, setCurrent] = useState(0);
-
   const onFinish = async (values) => {
     const { email } = values;
 
-    const eventSource = new EventSource(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/sse?email=${email}`
-    );
-
-    eventSource.onerror = () => {
-      message.error("Pretraga ne može da se sačuva, pokušajte kasnije");
-      setVisible(false);
-    };
-
-    eventBus.on("sse-close", () => eventSource.close());
-
-    eventSource.onmessage = async ({ data }) => {
-      const eventData = JSON.parse(data);
-      console.log("event data", eventData);
-      if (eventData.isVerifiedFilter) {
-        setCurrent(2);
-        eventSource.close();
-      }
-    };
-
     try {
       await filtersService.saveFilter({ ...filters, email });
+      message.info("Proveri mejl sa detaljima oko potvrde sačuvane pretrage");
     } catch {
       message.error("Pretraga nije sačuvana");
-      return;
     }
-
-    setCurrent(1);
   };
 
   const showModal = () => {
-    setCurrent(0);
     setVisible(true);
   };
 
   const closeModal = () => {
     setVisible(false);
     form.resetFields();
-    eventBus.dispatch("sse-close");
   };
 
   useEffect(() => {
@@ -110,46 +67,35 @@ export const TrackFiltersModal = () => {
         onCancel={closeModal}
         onOk={closeModal}
       >
-        <Steps direction="vertical" current={current}>
-          {steps.map((item) => (
-            <Step
-              key={item.title}
-              title={item.title}
-              description={item.description}
-            />
-          ))}
-        </Steps>
-        {current === 0 && (
-          <Form
-            name="email-form"
-            {...formItemLayout}
-            className="mb-5"
-            form={form}
-            onFinish={onFinish}
+        <Form
+          name="email-form"
+          {...formItemLayout}
+          className="mb-5"
+          form={form}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="email"
+            label="E-mail adresa"
+            className="mt-2"
+            hasFeedback
+            rules={[
+              {
+                type: "email",
+                message: "Uneta e-mail adresa nije ispravna!",
+              },
+              {
+                required: true,
+                message: "Unesi e-mail adresu!",
+              },
+            ]}
           >
-            <Form.Item
-              name="email"
-              label="E-mail adresa"
-              className="mt-2"
-              hasFeedback
-              rules={[
-                {
-                  type: "email",
-                  message: "Uneta e-mail adresa nije ispravna!",
-                },
-                {
-                  required: true,
-                  message: "Unesi e-mail adresu!",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Button type="primary" htmlType="submit">
-              Registruj se
-            </Button>
-          </Form>
-        )}
+            <Input />
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Registruj se
+          </Button>
+        </Form>
       </Modal>
     </>
   );
