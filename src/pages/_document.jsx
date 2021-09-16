591b0dda77098852b221ec7df72a95bc18eea2f1
zsevic <zsevic95@gmail.com>
2021-09-17 00:26:40 +0200

    Update icons

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -6,7 +6,7 @@ class CustomDocument extends Document {
     return (
       <Html lang="sr">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.svg" />
           <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#5bc0de" />
