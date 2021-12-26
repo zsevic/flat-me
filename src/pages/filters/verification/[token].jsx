4c20f05a23c4d2d55c23f7367c0d388a8938fd08
zsevic <zsevic95@gmail.com>
2021-12-26 14:38:01 +0100

    Update verification message

--- src/pages/filters/verification/[token].jsx
+++ src/pages/filters/verification/[token].jsx
@@ -5,7 +5,7 @@ import * as filtersService from "services/filters";
 const ERROR_MESSAGE = "Čuvanje pretrage nije uspelo";
 const SUCCESS_MESSAGE = "Uspešno sačuvana pretraga";
 const VERIFICATION_SUCCESS_MESSAGE =
-  "Dva puta dnevno ćete primati obaveštenja o novim stanovima na osnovu sačuvane pretrage.";
+  "Jednom dnevno ćete primati obaveštenja o novim stanovima na osnovu sačuvane pretrage.";
 
 const FilterVerificationPage = () => (
   <VerificationPage
