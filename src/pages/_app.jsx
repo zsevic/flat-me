38d04e4897af911dc8310bf63bf0efaf23a0f617
zsevic <zsevic95@gmail.com>
2022-02-03 15:53:25 +0100

    Improve code

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import React, { useEffect } from "react";
+import ReactGA from "react-ga";
 import "tailwindcss/tailwind.css";
 import "antd/dist/antd.css";
 import "../styles/globals.css";
-import { registerServiceWorker } from "services/service-worker-registration";
-import ReactGA from "react-ga";
 import { GOOGLE_ANALYTICS_TRACKING_ID } from "constants/config";
+import { registerServiceWorker } from "services/service-worker-registration";
 
 React.useLayoutEffect = React.useEffect;
 
