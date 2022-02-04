8b85b2b2cf644ea5dfc178ed0a43be81365f26e4
zsevic <zsevic95@gmail.com>
2022-02-04 05:45:32 +0100

    Track app installations

--- src/landing/components/button/installable.jsx
+++ src/landing/components/button/installable.jsx
@@ -31,6 +31,7 @@ export const InstallableButton = ({ sx, buttonId }) => {
 
     window.addEventListener("appinstalled", () => {
       setInstallable(false);
+      trackEvent("app-installed", "app-installed-successfully", "installation");
     });
   }, []);
 
