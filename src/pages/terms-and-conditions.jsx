4167ff0c96ca6307a51a6f6680f356c4e68c3701
zsevic <zsevic95@gmail.com>
2022-02-22 23:15:01 +0100

    Update terms and conditions

--- src/pages/terms-and-conditions.jsx
+++ src/pages/terms-and-conditions.jsx
@@ -98,65 +98,91 @@ const TermsAndConditionsPage = () => (
             </Heading>
             <Text as="p" sx={styles.subdescription}>
               Flat-me.com (u daljem tekstu: FlatMe platforma ili Platforma) je
-              internet stranica namenjena prikupljanju i objavljivanju oglasa iz
-              oblasti nekretnina. Korišćenjem FlatMe platforme i/ili bilo kog
-              njenog dela svaki pojedinačni posetilac i/ili korisnik (u daljem
-              tekstu: Korisnik) potvrđuje da je upoznat i saglasan sa odredbama
-              i uslovima navedenim u daljem tekstu. Uslovi korišćenja su
-              obavezujući.
+              internet stranica namenjena pretraživanju objavljenih oglasa,
+              odnosno javno dostupnih podataka iz oblasti prodaje ili zakupa
+              nepokretnosti, a sve u cilju obezbeđivanja mogućnosti korisnicima
+              da slobodno pristupaju i distribuiraju informacije putem internet
+              mreže, kao i da koriste aplikacije i usluge po svom izboru.
+              Korišćenjem FlatMe platforme ili bilo kog njenog dela, odnosno
+              svaka poseta ovoj internet stranici, prijavljivanje na FlatMe
+              obaveštenja, pretraga, kao i direktno dostavljanje podataka od
+              strane korisnika, znači da svaki pojedinačni posetilac, odnosno
+              korisnik (u daljem tekstu: Korisnik) potvrđuje da je upoznat i
+              saglasan u celosti sa odredbama navedenim u ovim Uslovima
+              korišćenja. U slučaju da su navedeni uslovi za Vas neprihvatljivi,
+              molimo Vas da ne koristite ovu internet stranicu.
             </Text>
             <Text as="p" sx={styles.subdescription}>
               FlatMe zadržava pravo promene Uslova korišćenja pri čemu iste
               stupaju na snagu odmah, objavljivanjem na ovoj stranici. FlatMe
-              platforma nije odgovorna, niti garantuje da su informacije na njoj
-              uvek aktuelne, ispravne i/ili potpune.
+              platforma nije odgovorna, niti garantuje da su informacije
+              pretražene na njoj uvek aktuelne, ispravne i/ili potpune.
             </Text>
             <Text as="p" sx={styles.subdescription}>
-              Datum poslednjeg ažuriranja: 6.2.2022.
+              Datum poslednjeg ažuriranja: 22.2.2022.
             </Text>
             <Heading sx={styles.subtitle}>OPŠTI USLOVI KORIŠĆENJA</Heading>
             <Text as="p" sx={styles.subdescription}>
-              FlatMe platforma je namenjena prikupljanju i objavljivanju oglasa
-              iz oblasti nekretnina. FlatMe nije agencija za izdavanje i/ili
-              prodaju nekretnina. FlatMe nema svojstvo, obaveze, ni odgovornosti
-              posrednika, niti platforma služi posredovanju između dva ili više
-              Korisnika u njihovim međusobnim transakcijama i trgovini.
+              Flat-me.com platforma na nekomercijalnoj osnovi prikuplja
+              informacije u vezi sa oglasima za prodaju ili zakup nekretnina,
+              objavljenih na internetu, te omogućava Korisniku Platforme
+              pretraživanje istih, na osnovu izabranih filtera. FlatMe nije
+              agencija za izdavanje i/ili prodaju nekretnina, te nije na bilo
+              koji način sponzorisana od strane trećih lica koja se javljaju u
+              svojstvu oglašivača oglasa ili prenosilaca oglasnih poruka, niti
+              je Platforma povezana sa istim licima. FlatMe nema svojstvo,
+              obaveze, ni odgovornosti posrednika prilikom prometa
+              nepokretnosti, niti Platforma učestvuje u posredovanju između dva
+              ili više Korisnika, oglašivača i ostalih lica u njihovim
+              međusobnim transakcijama i trgovini.
             </Text>
             <Text as="p" sx={styles.subdescription}>
-              FlatMe omogućava širokom krugu posetilaca i korisnika da pronađu i
-              pregledaju sve nekretnine na izabranoj teritoriji koji odgovaraju
-              zadatim parametrima, kao i da ostavljanjem svoje e-mail adrese
-              dobijaju obaveštenja o svim novoobjavljenim nekretninama koje
-              odgovaraju prethodno izabranim parametrima.
+              FlatMe omogućava širokom krugu posetilaca, odnosno korisnika sajta
+              da, bez ikakve naknade, pronađu i pregledaju nekretnine na
+              izabranoj teritoriji koji odgovaraju zadatim parametrima, kao i da
+              ostavljanjem svoje e-mail adrese dobijaju obaveštenja o svim
+              novoobjavljenim nekretninama koje odgovaraju prethodno izabranim
+              parametrima.
             </Text>
             <Text as="p" sx={styles.subdescription}>
               Korisnik može biti fizičko ili pravno lice. Korisnik snosi
               isključivu odgovornost i dužan je da obezbedi da korišćenje FlatMe
               platforme bude u skladu sa relevantnim propisima i odredbama ovih
-              Uslova korišćenja. FlatMe zadržava pravo da privremeno ili trajno
-              onemogući pristup Platformi u slučaju sumnje da Korisnik ne
-              ispunjava navedene uslove.
+              Uslova korišćenja. U slučaju sumnje da se pojedini Korisnik ne
+              pridržava ovih Uslova korišćenja, odnosno da korišćenjem Platforme
+              isti preduzima nedopuštene aktivnosti, FlatMe zadržava pravo da
+              Korisniku privremeno ili trajno onemogući pristup Platformi, kao i
+              da povodom navedenog obavesti nadležni državni organ, u skladu sa
+              relevantnim zakonskim odredbama.
             </Text>
             <Heading sx={styles.subtitle}>VEZA SA DRUGIM VEB PORTALIMA</Heading>
             <Text as="p" sx={styles.subdescription}>
-              FlatMe sadrži veze prema drugim internet stranicama. FlatMe nema
-              kontrolu nad tim stranicama i nije odgovoran za njihov sadržaj,
-              uključujući bez ograničenja vezu prema nekoj drugoj stranici, ili
-              izmene i ažuriranje takve stranice.
+              FlatMe sadrži veze, odnosno linkove prema drugim internet
+              stranicama. Platforma nema kontrolu nad tim stranicama i nije
+              odgovorna za njihov sadržaj, uključujući bez ograničenja vezu
+              prema nekoj drugoj stranici, ili izmene i ažuriranje takve
+              stranice. U slučaju da FlatMe dođe do saznanja da se radi o
+              nedopuštenom delovanju ili podatku, koji se može pretražiti na
+              ovoj internet stranici, Platforma će taj podatak, odnosno
+              informaciju ukolniti ili onemogućiti pristup istoj.
             </Text>
             <Text as="p" sx={styles.subdescription}>
               FlatMe nije odgovoran za istinitost, tačnost i verodostojnost
               informacija na povezanim stranicama, a naročito o svojstvu,
-              karakteristikama i stanju oglašene nekretnine. Odgovornost za
-              sadržaj poruke na povezanoj stranici je na licu od koga je takav
-              sadržaj potekao.
+              karakteristikama i stanju oglašene nekretnine, posebno imajući u
+              vidu da je Platforma isključivo namenjena pretraživanju podataka
+              koji su objavljeni i javno dostupni putem interneta. Odgovornost
+              za sadržaj poruke na povezanoj stranici je na licu od koga je
+              takav sadržaj potekao.
             </Text>
             <Text as="p" sx={styles.subdescription}>
-              FlatMe ne učestvuje u prodaji ili iznajmljivanju nekretnina koji
-              se oglašavaju na njoj, niti u bilo kojoj transakciji koja se
-              obavlja između korisnika stranice. FlatMe ne garantuje za bilo
-              koju nekretninu prikazanu na strani flat-me.com, uključujući
-              vlasništvo nad istom i spremnost korisnika da izvrši transakciju.
+              FlatMe je nekomercijalna platforma, te ne učestvuje u prodaji ili
+              iznajmljivanju nekretnina koje se oglašavaju na drugim internet
+              stranicama, niti učestvuje u bilo kojoj transakciji koja se
+              eventualno obavlja između korisnika stranice. FlatMe ne garantuje
+              za bilo koju nekretninu pretraženu i prikazanu na strani
+              flat-me.com, uključujući vlasništvo nad istom i spremnost
+              korisnika da izvrši transakciju.
             </Text>
             <Heading sx={styles.subtitle}>IZUZEĆE OD ODGOVORNOSTI</Heading>
             <Text as="p" sx={styles.subdescription}>
@@ -167,23 +193,24 @@ const TermsAndConditionsPage = () => (
               stranicu u bilo kojem trenutku.
             </Text>
             <Text as="p" sx={styles.subdescription}>
-              Sadržaj objavljen na ovoj stranici ne može biti temelj za
-              donošenje ličnih, pravnih ili finansijskih odluka. Korisnik
-              izričito izjavljuje da razume i prihvata da FlatMe ne snosi
-              nikakvu odgovornost za bilo kakvu štetu, uključujući, ali ne i
-              isključivo štete nastale usled gubitka profita, poslovne prilike,
-              korišćenja podataka ili drugih nematerijalnih gubitaka (čak i ako
-              je bio obavešten o mogućnosti nastanka takvih šteta ili je trebalo
-              da zna da takva mogućnost postoji), koje proističu iz korišćenja
-              ili nemogućnosti pristupa i korišćenja FlatMe platforme iz bilo
-              kog razloga.
+              FlatMe izričito napominje da ne snosi nikakvu odgovornost prema
+              Korisniku za donošenje njegovih ličnih, pravnih ili finansijskih
+              odluka na osnovu sadržaja pretraženog na ovoj stranici. Korisnik
+              izričito izjavljuje da razume i prihvata ove Uslove korišćenja, te
+              prihvata da FlatMe ne snosi nikakvu odgovornost za bilo kakvu
+              štetu, uključujući, ali ne i isključivo, štete nastale usled
+              gubitka profita, poslovne prilike, korišćenja podataka ili drugih
+              nematerijalnih gubitaka (naročito ako je bio obavešten o
+              mogućnosti nastanka takvih šteta ili je trebalo da zna da takva
+              mogućnost postoji), koje proističu iz korišćenja ili nemogućnosti
+              pristupa i korišćenja FlatMe platforme iz bilo kog razloga.
             </Text>
             <Text as="p" sx={styles.subdescription}>
               FlatMe ni u kojem slučaju ne može biti odgovoran za neposrednu,
               posrednu, slučajnu, posledičnu ili neku drugu štetu koja proizlazi
               ili je na bilo koji način povezana s korišćenjem platforme ili
               njenim radom. Navedeno se primenjuje u najvećem mogućem obimu koji
-              zakoni dozvoljavaju.
+              dozvoljavaju relevantne zakonske odredbe.
             </Text>
             <Heading sx={styles.subtitle}>
               NEZAKONITO ILI ZABRANJENO KORIŠĆENJE
@@ -205,9 +232,10 @@ const TermsAndConditionsPage = () => (
               FlatMe platforme, dostavi određene lične podatke čije
               prikupljanje, korišćenje, obrada i čuvanje je u skladu sa važećim
               propisima Republike Srbije i podležu Politici zaštite privatnosti
-              koja predstavlja sastavni deo ovih Uslova korišćenja. Korišćenje
-              FlatMe platforme ili bilo kog njenog dela podrazumeva da je
-              Korisnik pročitao, razumeo i složio se sa uslovima Politike
+              koja predstavlja sastavni deo ovih Uslova korišćenja. Posetom ove
+              internet stranice, odnosno korišćenjem FlatMe platforme ili bilo
+              kog njenog dela na bilo koji način, podrazumeva da je Korisnik
+              pročitao, razumeo i složio se u celosti sa uslovima Politike
               zaštite privatnosti koja je objavljena na adresi:{" "}
               <Link
                 path="https://www.flat-me.com/privacy-policy"
@@ -219,14 +247,15 @@ const TermsAndConditionsPage = () => (
               PREKID / OGRANIČENJE PRISTUPA
             </Heading>
             <Text as="p" sx={styles.subdescription}>
-              FlatMe zadržava diskreciono pravo da zabrani pristup Korisniku
-              koji na bilo koji način krši ove uslove korišćenja ili ometa rad
-              portala u bilo koje vreme i bez prethodnog obaveštenja, kao i
-              pravo da protiv Korisnika preduzme odgovarajuće mere.
+              FlatMe zadržava diskreciono pravo da u bilo koje vreme i bez
+              prethodnog obaveštenja, zabrani pristup Korisniku koji na bilo
+              koji način krši ove uslove korišćenja ili ometa rad portala, kao i
+              pravo da protiv Korisnika preduzme odgovarajuće mere, u skladu sa
+              zakonom.
             </Text>
             <Heading sx={styles.subtitle}>IZMENE USLOVA KORIŠĆENJA</Heading>
             <Text as="p" sx={styles.subdescription}>
-              FlatMe zadržava pravo da po sopstvenom nahođenju i bez prethodno
+              FlatMe zadržava pravo da po sopstvenom izboru i bez prethodno
               datog obveštenja, izvrši reviziju, ažurira, izmeni, dopuni ili
               izbriše određene odredbe ovih Uslova korišćenja. Ukoliko FlatMe
               izmeni Uslove korišćenja, izmena će biti dostupna na Internet
