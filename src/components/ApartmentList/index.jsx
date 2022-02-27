e453d1c03fee9d915d9826a73d16bae68246d9dc
zsevic <zsevic95@gmail.com>
2022-02-27 15:47:10 +0100

    Improve apartment list grid

--- src/components/ApartmentList/index.jsx
+++ src/components/ApartmentList/index.jsx
@@ -97,9 +97,9 @@ export const ApartmentList = ({
         grid={{
           gutter: 16,
           xs: 1,
-          sm: 2,
-          md: 3,
-          lg: 3,
+          sm: 1,
+          md: 2,
+          lg: 2,
           xl: 3,
           xxl: 3,
         }}
