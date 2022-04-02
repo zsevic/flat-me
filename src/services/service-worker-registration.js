ab579344493d4e0a098cbe0d4ae41d40f4e3ba96
zsevic <zsevic95@gmail.com>
2022-04-02 21:55:45 +0200

    Set scope for pwa

--- src/services/service-worker-registration.js
+++ src/services/service-worker-registration.js
@@ -2,7 +2,9 @@ export const registerServiceWorker = () => {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("/service-worker.js")
+        .register("/service-worker.js", {
+          scope: "/app",
+        })
         .then(() => console.log("Service worker is registered!"))
         .catch(() => console.error("Service worker registration failed"));
     });
