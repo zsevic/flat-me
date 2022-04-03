ec014d6e1e8623aaf88c78b8a88e4ad5b9205335
zsevic <zsevic95@gmail.com>
2022-04-03 22:31:37 +0200

    Improve scope field in manifest.json

--- src/services/service-worker-registration.js
+++ src/services/service-worker-registration.js
@@ -2,9 +2,7 @@ export const registerServiceWorker = () => {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("/service-worker.js", {
-          scope: "/app",
-        })
+        .register("/service-worker.js")
         .then(() => console.log("Service worker is registered!"))
         .catch(() => console.error("Service worker registration failed"));
     });
