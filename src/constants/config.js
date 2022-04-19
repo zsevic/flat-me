c9c7c344da5a29302ee7f01cf01520185e486cb4
zsevic <zsevic95@gmail.com>
2022-04-19 23:03:09 +0200

    Update firebase config

--- src/constants/config.js
+++ src/constants/config.js
@@ -41,13 +41,7 @@ export const INITIAL_FILTERS = {
 };
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyC-I6PYtZwd9iiBlceEKqzHIPexlT-X4iA",
-  authDomain: "flat-me-2b34d.firebaseapp.com",
-  projectId: "flat-me-2b34d",
-  storageBucket: "flat-me-2b34d.appspot.com",
   messagingSenderId: "238414106033",
-  appId: "1:238414106033:web:e38eca44a89cfe3d0ec190",
-  measurementId: "G-1F2ED6J80T",
 };
 
 export const TRACK_FILTERS_MODAL_TITLE = "Uključi email obaveštenja";
