8b881c72bd33ff0367cd72e9e2cdbd903bdfd4ae
zsevic <zsevic95@gmail.com>
2022-04-20 18:12:53 +0200

    Fix Firebase config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,7 +3,13 @@ importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
 firebase.initializeApp({
+  apiKey: "AIzaSyC-I6PYtZwd9iiBlceEKqzHIPexlT-X4iA",
+  authDomain: "flat-me-2b34d.firebaseapp.com",
+  projectId: "flat-me-2b34d",
+  storageBucket: "flat-me-2b34d.appspot.com",
   messagingSenderId: "238414106033",
+  appId: "1:238414106033:web:e38eca44a89cfe3d0ec190",
+  measurementId: "G-1F2ED6J80T",
 });
 // eslint-disable-next-line
 const messaging = firebase.messaging();
