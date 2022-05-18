760707acf02bcf6607f402264fcb2c1f6000f41f
zsevic <zsevic95@gmail.com>
2022-05-18 18:23:21 +0200

    Update notification footer

--- src/components/NotificationFooter/index.jsx
+++ src/components/NotificationFooter/index.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export const NotificationFooter = () => (
   <div>
     <small>
-      Promenom obaveštenja, slažete sa FlatMe{" "}
+      Aktiviranjem obaveštenja, slažete sa FlatMe{" "}
       <Link href="/terms-and-conditions" passHref>
         <a target="_blank" rel="noopener noreferrer">
           Uslovima korišćenja
