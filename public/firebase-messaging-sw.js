72f68cc00eee46632c676918e7809de37ccfc3f1
zsevic <zsevic95@gmail.com>
2022-06-05 15:02:25 +0200

    Improve Firebase messaging service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -11,24 +11,26 @@ self.addEventListener("notificationclick", (event) => {
   );
 });
 
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
-);
-
 let messaging = null;
 try {
-  firebase.initializeApp({
-    apiKey: "AIzaSyC-I6PYtZwd9iiBlceEKqzHIPexlT-X4iA",
-    authDomain: "flat-me-2b34d.firebaseapp.com",
-    projectId: "flat-me-2b34d",
-    storageBucket: "flat-me-2b34d.appspot.com",
-    messagingSenderId: "238414106033",
-    appId: "1:238414106033:web:e38eca44a89cfe3d0ec190",
-    measurementId: "G-1F2ED6J80T",
-  });
+  if (typeof importScripts === "function") {
+    importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+    importScripts(
+      "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+    );
 
-  messaging = firebase.messaging();
+    firebase.initializeApp({
+      apiKey: "AIzaSyC-I6PYtZwd9iiBlceEKqzHIPexlT-X4iA",
+      authDomain: "flat-me-2b34d.firebaseapp.com",
+      projectId: "flat-me-2b34d",
+      storageBucket: "flat-me-2b34d.appspot.com",
+      messagingSenderId: "238414106033",
+      appId: "1:238414106033:web:e38eca44a89cfe3d0ec190",
+      measurementId: "G-1F2ED6J80T",
+    });
+
+    messaging = firebase.messaging();
+  }
 } catch (error) {
   console.error(error);
 }
