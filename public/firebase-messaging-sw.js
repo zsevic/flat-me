453da9b1b7392ed7a8bf0a0e3922febc9d227d7a
zsevic <zsevic95@gmail.com>
2022-06-05 14:02:07 +0200

    Fix Firebase messaging service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -15,18 +15,19 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
-firebase.initializeApp({
-  apiKey: "AIzaSyC-I6PYtZwd9iiBlceEKqzHIPexlT-X4iA",
-  authDomain: "flat-me-2b34d.firebaseapp.com",
-  projectId: "flat-me-2b34d",
-  storageBucket: "flat-me-2b34d.appspot.com",
-  messagingSenderId: "238414106033",
-  appId: "1:238414106033:web:e38eca44a89cfe3d0ec190",
-  measurementId: "G-1F2ED6J80T",
-});
 
 let messaging = null;
 try {
+  firebase.initializeApp({
+    apiKey: "AIzaSyC-I6PYtZwd9iiBlceEKqzHIPexlT-X4iA",
+    authDomain: "flat-me-2b34d.firebaseapp.com",
+    projectId: "flat-me-2b34d",
+    storageBucket: "flat-me-2b34d.appspot.com",
+    messagingSenderId: "238414106033",
+    appId: "1:238414106033:web:e38eca44a89cfe3d0ec190",
+    measurementId: "G-1F2ED6J80T",
+  });
+
   messaging = firebase.messaging();
 } catch (error) {
   console.error(error);
